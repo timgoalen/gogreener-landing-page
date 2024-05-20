4ef0e4a2dd2fc17e5f3c4c83c9f76aa301430d83
Tim Goalen <timgoalen@hotmail.com>
2024-05-20 08:55:53 +0000

    refactor(main.go): remove redundant code and add clarifying comments

--- main.go
+++ main.go
@@ -2,102 +2,33 @@ package main
 
 import (
 	"context"
-	"embed"
-	"html/template"
-	"io/fs"
 	"log"
-	"net/http"
 
 	"github.com/thejimmyg/greener"
 )
 
-var wwwFiles embed.FS
-
-type SimpleConfig struct{}
-
-func NewSimpleConfig() *SimpleConfig {
-	return &SimpleConfig{}
-}
-
-// An example of injecting a component which needs both the SimpleApp and SimpleServices
-type WritePageProvider interface {
-	WritePage(title string, body template.HTML)
-}
-
-type DefaultWritePageProvider struct {
-	greener.ResponseWriterProvider
-	greener.EmptyPageProvider
-}
-
-func (d *DefaultWritePageProvider) WritePage(title string, body template.HTML) {
-	d.W().Write([]byte(d.Page(title, body)))
-}
-
-func NewDefaultWritePageProvider(emptyPageProvider greener.EmptyPageProvider, responseWriterProvider greener.ResponseWriterProvider) *DefaultWritePageProvider {
-	return &DefaultWritePageProvider{EmptyPageProvider: emptyPageProvider, ResponseWriterProvider: responseWriterProvider}
-}
-
-type SimpleServices struct {
-	greener.Services
-	WritePageProvider // Here is the interface we are extending the serivces with
-}
-
-type SimpleApp struct {
-	greener.App
-	*SimpleConfig
-}
-
-func NewSimpleApp(app greener.App, simpleConfig *SimpleConfig) *SimpleApp {
-	return &SimpleApp{
-		App:          app,
-		SimpleConfig: simpleConfig,
-	}
-}
-
-func (app *SimpleApp) HandleWithSimpleServices(path string, handler func(*SimpleServices)) {
-	app.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
-		services := app.NewServices(w, r)
-		s := &SimpleServices{Services: services} // We have to leave WritePageProvider nil temporarily
-		writePageProvider := NewDefaultWritePageProvider(app, s)
-		s.WritePageProvider = writePageProvider // Now we set it here.
-		handler(s)
-	})
-}
-
 func main() {
-	// Setup
-	wwwFS, _ := fs.Sub(wwwFiles, "www") // Used for the icon and the static file serving
-	uiSupport := []greener.UISupport{greener.NewDefaultUISupport(
-		GlobalCSS,
-		`console.log("Hello from script");`,
-		`console.log("Hello from service worker");`,
-	)}
-	themeColor := "#000000"
-	appShortName := "Simple"
-	config := greener.NewDefaultServeConfigProviderFromEnvironment()
-	logger := greener.NewDefaultLogger(log.Printf)
-	injectors := []greener.Injector{
-		greener.NewDefaultStyleInjector(logger, uiSupport),
-		greener.NewDefaultScriptInjector(logger, uiSupport),
-		greener.NewDefaultServiceWorkerInjector(logger, uiSupport),
-		greener.NewDefaultThemeColorInjector(logger, themeColor),
-		greener.NewDefaultIconsInjector(logger, wwwFS),
-		greener.NewDefaultManifestInjector(logger, appShortName),
-	}
-	emptyPageProvider := greener.NewDefaultEmptyPageProvider(injectors)
-	static := greener.NewCompressedFileHandler(http.FS(wwwFS))
+	// Create UI Support (args: CSS, JavaScript, Service Worker)
+	uiSupport := greener.NewDefaultUISupport(GlobalCSS, "", "")
 
-	// Routes
-	app := NewSimpleApp(greener.NewDefaultApp(config, logger, emptyPageProvider), NewSimpleConfig())
-	app.HandleWithSimpleServices("/", func(s *SimpleServices) {
-		if s.R().URL.Path != "/" {
-			// If no other route is matched and the request is not for / then serve a static file
-			static.ServeHTTP(s.W(), s.R())
-		} else {
-			s.WritePage("GoGreener", greener.HTMLPrintf(HomePageHTML))
-		}
+	// Set up logging
+	logger := greener.NewDefaultLogger(log.Printf)
+
+	// Set up the application
+	app := greener.NewDefaultApp(
+		greener.NewDefaultServeConfigProviderFromEnvironment(),
+		logger,
+		greener.NewDefaultEmptyPageProvider([]greener.Injector{
+			// Inject CSS
+			greener.NewDefaultStyleInjector(logger, []greener.UISupport{uiSupport}),
+		}),
+	)
+
+	// Define a simple route handler
+	app.HandleWithServices("/", func(s greener.Services) {
+		s.W().Write([]byte(app.Page("GoGreener", greener.HTMLPrintf(HomePageHTML))))
 	})
 
-	// Serve
+	// Start the server
 	app.Serve(context.Background())
 }
